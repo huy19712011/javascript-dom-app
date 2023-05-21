1de477f1ccc604eeb8bddf8351440afb4dc2074e
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-05-21 22:41:15 +0700

    Structuring our HTML template

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 
 const app = document.getElementById('app');
 
-app.innerHTML = `Hello`;
+app.innerHTML = `
+    <div class="todos">
+        <div class="todos-header">
+            <h3 class="todos-title">Todo List</h3>
+            <div>
+                <p>You have <span class="todos-count"></span> items</p>
+                <button type="button" class="todos-clear" style="display: none;">
+                    Clear Completed todos 
+                </button>
+            </div>
+        </div>
+        <form class="todos-form" name="todos">
+            <input type="text" placeholder="What is next?" name="todo">
+        </form>
+        <ul class="todos-list"></ul>
+    </div>
+`;
